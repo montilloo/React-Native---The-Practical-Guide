ec09c3f2334952621e5e4d675cfa92ef2495d7f1
montilloo <nikun2008@126.com>
2021-11-17 14:26:13 +0800

    Finishing the Modal Styling;

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,8 +22,14 @@ const GoalInput = props => {
 					onChangeText={goalInputHandler}
 					value={enteredGoal}
 				/>
-				<Button title='取消' onPress={props.onCancelAdditional}/>
-				<Button title="添加" onPress={goalAddHandler}/>
+				<View style={styles.buttonContainer}>
+					<View style={styles.button}>
+						<Button title='取消' onPress={props.onCancelAdditional}/>
+					</View>
+					<View style={styles.button}>
+						<Button title="添加" onPress={goalAddHandler}/>
+					</View>
+				</View>
 			</View>
 		</Modal>
 	);
@@ -31,7 +37,15 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
 	inputContainer: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-	input: {width: '80%', borderColor: "black", borderWidth: 1, padding: 10}
+	input: {width: '80%', borderColor: "black", borderWidth: 1, padding: 10},
+	buttonContainer: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		width: '60%'
+	},
+	button: {
+		width: '40%'
+	}
 });
 
 export default GoalInput;
